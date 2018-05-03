2a58581bd124b14e5a64de1768c0d47568899a4a
Lizzie <lizziecanamar@gmail.com>
2018-05-02 20:54:29 -0500

    cambiar table semana a una nueva view

--- SaludIntegral/AgregarActividadViewController.swift
+++ SaludIntegral/AgregarActividadViewController.swift
@@ -9,63 +9,122 @@
 import UIKit
 import CoreData
 
-class AgregarActividadViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITableViewDelegate, UITableViewDataSource {
+class AgregarActividadViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, protocoloDiasSemana{
+    func ponerDias(diasSeleccionados: [Bool]) {
+        //
+    }
+    
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var pickerFechaUnica: UIDatePicker!
     @IBOutlet weak var tfNombre: UITextField!
-    
     @IBOutlet weak var tipoActividad: UISegmentedControl!
-    
     @IBOutlet weak var horaAlarma: UIDatePicker!
+    @IBOutlet weak var elegirDias: UIButton!
     
-    @IBOutlet weak var tablaSemana: UITableView!
+    //@IBOutlet weak var weekTable: UITableView!
     
     var loGuardo = false
     var nuevaActividad: Actividad!
-    
     var arrayFrecuencias = ["Una vez", "Semanal"]
-    var diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
+    
     var diasSeleccionados = [false, false, false, false, false, false, false]
     
     var frecuenciaSeleccionada: String = ""
+    @IBOutlet weak var pickerFrecuencia: UIPickerView!
     
     @IBAction func quitaTeclado() {
         view.endEditing(true)
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loGuardo = false
+        let tap = UITapGestureRecognizer(target: self, action: #selector(quitaTeclado))
+        view.addGestureRecognizer(tap)
+        scrollView.contentSize = contentView.frame.size
+        pickerFrecuencia.delegate = self
+        pickerFrecuencia.dataSource = self
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        if(nuevaActividad == nil) {
+            nuevaActividad = NSEntityDescription.insertNewObject(forEntityName: "Actividad", into: context) as! Actividad
+        }
+        tfNombre.text = nuevaActividad.titulo
+        frecuenciaSeleccionada = arrayFrecuencias[Int(nuevaActividad.tipoFrecuencia)]
+        pickerFrecuencia.selectRow(Int(nuevaActividad.tipoFrecuencia), inComponent: 0, animated: true)
+        actualizarFrecuencia()
+        if let frecuenciaSemana = nuevaActividad.frecuenciaSemana {
+            diasSeleccionados = frecuenciaSemana
+        }
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    //TableView
+    
+   /* func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return diasSemana.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tablaSemana.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        let cell = weekTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
         let dia = diasSemana[indexPath.row]
         cell.textLabel?.text = dia as String
-        if diasSeleccionados[indexPath.row] {
+        if diasSeleccionados[indexPath.row]{
             cell.accessoryType = UITableViewCellAccessoryType.checkmark
-        } else {
+        }else{
             cell.accessoryType = UITableViewCellAccessoryType.none
         }
-        //tablaSemana.reloadData()
         return cell
-        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         diasSeleccionados[indexPath.row] = !diasSeleccionados[indexPath.row]
-        if diasSeleccionados[indexPath.row] {
-            tablaSemana.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.checkmark
-        } else {
-            tablaSemana.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.none
+        if diasSeleccionados[indexPath.row]{
+            weekTable.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.checkmark
+        }else{
+            weekTable.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.none
         }
-        tablaSemana.reloadData()
+        
+    }*/
+    //Picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return arrayFrecuencias.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return arrayFrecuencias[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        frecuenciaSeleccionada = arrayFrecuencias[row]
+        actualizarFrecuencia()
+    }
+    
+    func actualizarFrecuencia() {
+        switch frecuenciaSeleccionada {
+        case arrayFrecuencias[0]:
+            pickerFechaUnica.isHidden = false
+            elegirDias.isHidden = true
+            break
+        case arrayFrecuencias[1]:
+            pickerFechaUnica.isHidden = true
+            elegirDias.isHidden = false
+            break
+        default:
+            break
+        }
+    }
+    
+    
     
     @IBAction func agregarActividad(_ sender: UIButton) {
         loGuardo = true
@@ -97,76 +156,9 @@ class AgregarActividadViewController: UIViewController, UIPickerViewDelegate, UI
             alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
-        
-        //navigationController?.popViewController(animated: true)
-        
     }
     
-    @IBOutlet weak var pickerFrecuencia: UIPickerView!
    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        loGuardo = false
-        let tap = UITapGestureRecognizer(target: self, action: #selector(quitaTeclado))
-        view.addGestureRecognizer(tap)
-        scrollView.contentSize = contentView.frame.size
-        pickerFrecuencia.delegate = self
-        pickerFrecuencia.dataSource = self
-        
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        
-        if(nuevaActividad == nil) {
-            nuevaActividad = NSEntityDescription.insertNewObject(forEntityName: "Actividad", into: context) as! Actividad
-        }
-        tfNombre.text = nuevaActividad.titulo
-        frecuenciaSeleccionada = arrayFrecuencias[Int(nuevaActividad.tipoFrecuencia)]
-        pickerFrecuencia.selectRow(Int(nuevaActividad.tipoFrecuencia), inComponent: 0, animated: true)
-        actualizarFrecuencia()
-        if let frecuenciaSemana = nuevaActividad.frecuenciaSemana {
-            diasSeleccionados = frecuenciaSemana
-        }
-        tablaSemana.reloadData()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return arrayFrecuencias.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return arrayFrecuencias[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        frecuenciaSeleccionada = arrayFrecuencias[row]
-        actualizarFrecuencia()
-    }
-    
-
-    
-    func actualizarFrecuencia() {
-        switch frecuenciaSeleccionada {
-        case arrayFrecuencias[0]:
-            tablaSemana.isHidden = true
-            pickerFechaUnica.isHidden = false
-            break
-        case arrayFrecuencias[1]:
-            tablaSemana.isHidden = false
-            pickerFechaUnica.isHidden = true
-            break
-        default:
-            break
-        }
-    }
-    
-
 
    override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -179,6 +171,14 @@ class AgregarActividadViewController: UIViewController, UIPickerViewDelegate, UI
                 try context.save()
             } catch _ {
             }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "tableWeekDays"{
+            let vistaTabla = segue.destination as! TableViewController
+            vistaTabla.semana = diasSeleccionados
+            
         }
     }
  
