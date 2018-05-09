560d04886a61155d24c4e3bae0bd5dd6e46bd584
Uriel96 <usu.96@hotmail.com>
2018-05-08 21:22:28 -0500

    Update DiasSemanaViewController.swift

--- DiasSemanaViewController.swift
+++ DiasSemanaViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
+/// Controlador que permite seleccionar los dias de la semana que se quiere realizar la actividad.
 class DiasSemanaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    /// No permite que el dispositivo se rote.
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.landscape
     }
@@ -17,13 +19,16 @@ class DiasSemanaViewController: UIViewController, UITableViewDelegate, UITableVi
         return false
     }
     
+    /// Contiene las opciones de los dias de la semana.
     var diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
+    /// Contiene los dias de la semana que se realizara la actividad.
     var diasSeleccionados = [Bool]()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return diasSemana.count
     }
     
+    /// Agrega a las celdas un check para seleccionar los dias.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let dia = diasSemana[indexPath.row]
@@ -37,6 +42,7 @@ class DiasSemanaViewController: UIViewController, UITableViewDelegate, UITableVi
         return cell
     }
     
+    /// Selecciona o deselecciona una dia de la semana.
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         diasSeleccionados[indexPath.row] = !diasSeleccionados[indexPath.row]
          if diasSeleccionados[indexPath.row] {
@@ -61,8 +67,7 @@ class DiasSemanaViewController: UIViewController, UITableViewDelegate, UITableVi
 
     
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    /// Pasa los dias seleccionados a agregar actividad.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
